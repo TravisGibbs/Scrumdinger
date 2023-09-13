2bdd775ff66dbc1e092f321d24b9b43fea8749ee
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-09-13 13:48:20 -0500

    Chapter 6

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -4,9 +4,12 @@
 
 import SwiftUI
 
+
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
-  
+    @State private var isPresentingNewScrumView = false
+
+
     var body: some View {
         NavigationStack {
             List($scrums) { $scrum in
@@ -17,14 +20,20 @@ struct ScrumsView: View {
             }
             .navigationTitle("Daily Scrums")
             .toolbar {
-                Button(action: {}) {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
                     Image(systemName: "plus")
                 }
                 .accessibilityLabel("New Scrum")
             }
         }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+        }
     }
 }
+
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
